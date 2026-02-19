a6acb00943a0fe7b80e5e71cfea0dd9f41511637
WenchaoZhu0128 <wz2754@columbia.edu>
2026-02-18 21:55:30 -0500

    Correct FizzBuzz function syntax errors
    Fix syntax errors in FizzBuzz function.

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -3,8 +3,8 @@
 def FizzBuzz(start, finish):
     outlist = []
 
-    for i in range(start, finish + 1)
-        if i % 3 == 0 and i % 5 = 0:
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
             outlist.append("fizzbuzz")
         elif i % 3 == 0:
             outlist.qppend("fizz")
