733fbceb9a0eba48d39df200e939c1361a08c13f
WenchaoZhu0128 <wz2754@columbia.edu>
2026-02-18 20:26:38 -0500

    Refactor getBondPrice to calculate bond price
    Refactor getBondPrice function to calculate bond price based on yield, face value, coupon rate, and maturity with support for different payment periods.

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,16 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    y = y / ppy
+    couponRate = couponRate / ppy
+    m = m * ppy
+    pvcfsum = 0
+
+    for t in range(1, m+1):
+        pvm = (1+y)**(-t)
+        cf = face * couponRate
+        if t == m:
+            cf = cf + face
+        pvcf = pvm * face
+        pvcfsum = pvcfsum + pvcf
+    return pvcfsum
