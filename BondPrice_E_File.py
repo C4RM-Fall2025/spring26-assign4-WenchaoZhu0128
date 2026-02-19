403e369a72fb995075936f06e4f55b61b9e6870c
WenchaoZhu0128 <wz2754@columbia.edu>
2026-02-18 21:07:08 -0500

    Add bond price calculation in getBondPrice_E function
    Implement bond price calculation using present value of cash flows.

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,15 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    pvcfsum = 0
+    m = len(yc)
+
+    for t in range(1, m+1):
+        y = yc[t-1]
+        pvm = (1+y)**(-t)
+        cf = face * couponRate
+        if t == m:
+            cf = cf + face
+        pvcf = pvm * cf
+        pvcfsum = pvcfsum + pvcf
+    return(pcvfsum)
